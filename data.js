1fc2e6702855df9169326a1a0898e64fba5c2980
SheillaO <sheilla.alusa@strathmore.edu>
2026-04-22 14:22:10 +0300

    Update data.js

--- data.js
+++ data.js
@@ -29,3 +29,31 @@ export const tweetsData = [
                 profilePic: `images/chucknorris.jpeg`,
                 tweetText: `I went last year😴`,
             },
+             ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@NoobCoder12`,
+        profilePic: `images/flower.png`,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/overflow.png`,
+                tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
+            },
+            {
+                handle: `@YummyCoder64`,
+                profilePic: `images/love.png`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },      
+  
